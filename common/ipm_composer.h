ad771a958d71b5dde34bb8c553128c9fb88705ee
hugo.liu <hugo.liu@siengine.com>
2022-10-20 14:16:18 +0800

    using 1080*360 ipm
    Signed-off-by: hugo.liu <hugo.liu@siengine.com>

--- common/ipm_composer.h
+++ common/ipm_composer.h
@@ -34,19 +34,30 @@ class IpmComposer
 {
 public:
     static constexpr float DT_THRESHOLD_SEC_ = 0.02f;
-    static constexpr int BEV_H_ = 1280;
-    static constexpr int BEV_W_ = 1280;
-    static constexpr float BEV_XMAX_ = 32.0;
-    static constexpr float BEV_YMAX_ = 32.0;
-    /* define center point of cameras */
-    static constexpr float SVC_FRONT_X0 = 640.0f;
-    static constexpr float SVC_FRONT_Y0 = 536.8f;
-    static constexpr float SVC_LEFT_X0 = 596.0f;
-    static constexpr float SVC_LEFT_Y0 = 636.8f;
-    static constexpr float SVC_REAR_X0 = 640.0f;
-    static constexpr float SVC_REAR_Y0 = 744.8f;
-    static constexpr float SVC_RIGHT_X0 = 684.0f;
-    static constexpr float SVC_RIGHT_Y0 = 636.8f;
+    static constexpr int BEV_H_ = 1080;
+    static constexpr int BEV_W_ = 360;
+    static constexpr float BEV_XMAX_ = 54.0;
+    static constexpr float BEV_YMAX_ = 18.0;
+    /* define center point of cameras, 1280*1280, 32*32m */
+    // static constexpr float SVC_FRONT_X0 = 640.0f;
+    // static constexpr float SVC_FRONT_Y0 = 536.8f;
+    // static constexpr float SVC_LEFT_X0 = 596.0f;
+    // static constexpr float SVC_LEFT_Y0 = 636.8f;
+    // static constexpr float SVC_REAR_X0 = 640.0f;
+    // static constexpr float SVC_REAR_Y0 = 744.8f;
+    // static constexpr float SVC_RIGHT_X0 = 684.0f;
+    // static constexpr float SVC_RIGHT_Y0 = 636.8f;
+
+    /* define center point of cameras, 1080*360, 54*18m */
+    static constexpr float SVC_FRONT_X0 = 180.0f;
+    static constexpr float SVC_FRONT_Y0 = 488.4f;
+    static constexpr float SVC_LEFT_X0 = 158.0f;
+    static constexpr float SVC_LEFT_Y0 = 538.4f;
+    static constexpr float SVC_REAR_X0 = 180.0f;
+    static constexpr float SVC_REAR_Y0 = 592.4f;
+    static constexpr float SVC_RIGHT_X0 = 202.0f;
+    static constexpr float SVC_RIGHT_Y0 = 538.4f;
+
 
 private:
     ros::Publisher pub_image_ipm_;
@@ -103,7 +114,7 @@ public:
         homo_svc_right_ = homo_svc_right;
         ROS_INFO("homo_svc_right_: %s", ViwoUtils::CvMat2Str(homo_svc_right_).c_str());
 
-        GenIpmMasksBow();
+        GenIpmMasksBow2();
     }
 
     ImageQueuePtr_t GetSvcBuffer(SvcIndex_t idx){
@@ -255,6 +266,62 @@ public:
 
 private:
     /* 
+      line_left_forward: svc_left_center, (0, R*h)
+      line_right_forward: svc_right_center, (w, R*h)
+      line_left_rear: svc_left_center, (0, (1-R)*h)
+      line_right_rear: svc_right_center, (w, (1-R)*h)
+      k = (y2-y1)/(x1-x2)
+      b = (x2y1-x1y2)/(x1-x2)
+     */
+    void GenIpmMasksBow2(){
+        //ratio of height start-point, 0.0~0.5
+        const float R = 0.27f; 
+
+        /* line_left_forward */
+        float k_lf, b_lf;
+        GetKBFromTwoPoints(SVC_LEFT_X0, SVC_LEFT_Y0, 0.0f, R*BEV_H_, k_lf, b_lf);
+        /* line_right_forward */
+        float k_rf, b_rf;
+        GetKBFromTwoPoints(SVC_RIGHT_X0, SVC_RIGHT_Y0, BEV_W_, R*BEV_H_, k_rf, b_rf);
+        /* line_left_rear */
+        float k_lr, b_lr;
+        GetKBFromTwoPoints(SVC_LEFT_X0, SVC_LEFT_Y0, 0, (1.0f-R)*BEV_H_, k_lr, b_lr);
+        /* line_right_rear */
+        float k_rr, b_rr;
+        GetKBFromTwoPoints(SVC_RIGHT_X0, SVC_RIGHT_Y0, BEV_W_, (1.0f-R)*BEV_H_, k_rr, b_rr);
+
+        for(int y=0; y<BEV_H_; y++){
+            for(int x=0; x<BEV_W_; x++){
+                float line_lf = k_lf*x + y + b_lf;
+                float line_rf = k_rf*x + y + b_rf;
+                float line_lr = k_lr*x + y + b_lr;
+                float line_rr = k_rr*x + y + b_rr;
+                /* svc_front area */
+                if(line_lf<=0.0f && line_rf<0.0f && y<SVC_FRONT_Y0){
+                    ipm_mask_svc_front_.at<uchar>(y, x) = 0;
+                }
+                /* svc_left area */
+                else if(line_lf>0.0f && line_lr<=0.0f){
+                    ipm_mask_svc_left_.at<uchar>(y, x) = 0;
+                }
+                /* svc_rear area */
+                else if(line_lr>=0.0f && line_rr>0.0f && y>SVC_REAR_Y0){
+                    ipm_mask_svc_rear_.at<uchar>(y, x) = 0;
+                }
+                /* svc_right area */
+                else if(line_rr<0.0f && line_rf>=0.0f){
+                    ipm_mask_svc_right_.at<uchar>(y, x) = 0;
+                }
+            }
+        }
+        // std::string filepath = "/home/hugoliu/github/catkin_ws/src/VIW-Fusion/output";
+        // cv::imwrite((filepath+"/svc_front_mask.png").c_str(), 255*ipm_mask_svc_front_);
+        // cv::imwrite((filepath+"/svc_left_mask.png").c_str(), 255*ipm_mask_svc_left_);
+        // cv::imwrite((filepath+"/svc_rear_mask.png").c_str(), 255*ipm_mask_svc_rear_);
+        // cv::imwrite((filepath+"/svc_right_mask.png").c_str(), 255*ipm_mask_svc_right_);
+    }
+
+    /* 
       line_left_forward: svc_left_center, (R*w, 0)
       line_right_forward: svc_right_center, ((1-R)*w, 0)
       line_left_rear: svc_left_center, (R*w, h)
@@ -262,7 +329,7 @@ private:
       k = (y2-y1)/(x1-x2)
       b = (x2y1-x1y2)/(x1-x2)
      */
-    void GenIpmMasksBow(){
+    void GenIpmMasksBow1(){
         //ratio of width start-point, 0.0~0.5
         const float R = 0.16f; 
 
